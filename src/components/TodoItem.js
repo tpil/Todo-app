be6c32e959f6d0e71b5d0120fa0d9815e2a999ab
teopyl <teopyl@gmail.com>
2020-11-21 22:58:37 +0200

    Converted TodoList to Class

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,25 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './TodoItem.css';
 import { Container,Row,Col,Button } from 'react-bootstrap';
 
- const TodoItem = (props) =>{
+ class TodoItem extends Component{
+        constructor(props){
+            super(props);
+            this.state={
+                todoCompletion:this.props.completed,
+                
+            }
+        }
+       /* let completionCheck = () =>{
+            if (props.completed === true){
+                return {textDecoration:'line-through'}
+            }else{
+                return {textDecoration:'none'}
+        }
+*/
+    toggleComplete = (e) =>{
+        if (this.state.todoCompletion === false){
+            this.setState({todoCompletion:true});
+           
+        }else{
+            this.setState({todoCompletion:false});
+          
+        }
 
+    }
+    componentDidUpdate() {
+      if (this.state.todoCompletion === true){
+            return {textDecoration:"line-through"}
+      }else{
+        return {textDecoration:"none"}
+      }
 
-
-    return (
-      <Container>
-            <Row >
-                <Col className="col-11">
-                    <p className="TodoItem" >
-                    <input type="checkbox" /> <b>{props.Todo} </b>
-                    </p>
-                </Col>
-                <Col >
-                    <Button className="delete-btn"variant="danger">Delete</Button>
-                </Col>
-            </Row>
-        </Container>
- 
-    );
+    }
+    render(){
+        return (
+        <Container>
+                <Row >
+                    <Col className="col-11">
+                        <p className="TodoItem" >
+                        <input type="checkbox" onChange={this.toggleComplete}/>           
+                        <span style={{paddingLeft:'10px'}}><b style={this.componentDidUpdate()}>{this.props.Todo}</b></span>
+                        </p>
+                    </Col>
+                    <Col >
+                        <Button className="delete-btn"variant="danger">Delete</Button>
+                    </Col>
+                </Row>
+            </Container>
+    
+        );
+    }
 }
 export default TodoItem;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import TodoItem from './TodoItem';
+
+const todoItems = [
+    {
+        id:1,
+        item:"wash car",
+        completed:false
+    },
+    {
+        id:2,
+        item:"Cook",
+        completed:false
+    },
+    {
+        id:3,
+        item:"put the trash out",
+        completed:false
+    },
+    {
+        id:4,
+        item:"walk the dog",
+        completed:false
+    },
+    {
+        id:5,
+        item:"Do the dishes",
+        completed:false
+    }
+
+];
+
 const TodoList = () => {
+
+const todosArray = todoItems.map((todo)=>{
+    return <TodoItem key={todo.id}  Todo={todo.item} completed={todo.completed} />
+
+});
+
+
     return (
         <div className="list-container"style={{margin:'3px'}}>
-             <TodoItem Todo="wash car" />
-             <TodoItem Todo="cook"/>
-             <TodoItem Todo="put the trash out"/>
+             {todosArray}
         </div>
     )
 }
