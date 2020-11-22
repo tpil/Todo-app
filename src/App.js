e789c6668e7f1097f7e34ce6ce73ab36964b99a9
teopyl <teopyl@gmail.com>
2020-11-22 15:07:32 +0200

    AddNewToDo.js, uuid npm installation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React,{Component} from 'react';
-
-import AddNewToDo from './components/addNewToDo';
+import AddNewToDo from './components/AddNewToDo';
 import TodoList from './components/TodoList';
 import './App.css';
 
-
+import { v4 as uuidv4 } from 'uuid'; //to create random ids
 
 class App extends Component {
 
@@ -13,27 +12,27 @@ constructor(){
   this.state={
     todo : [
         {
-            id:1,
+            id:uuidv4(),
             item:"wash car",
             completed:false
         },
         {
-            id:2,
+            id:uuidv4(),
             item:"Cook",
             completed:false
         },
         {
-            id:3,
+            id:uuidv4(),
             item:"put the trash out",
             completed:false
         },
         {
-            id:4,
+            id:uuidv4(),
             item:"walk the dog",
             completed:false
         },
         {
-            id:5,
+            id:uuidv4(),
             item:"Do the dishes",
             completed:false
         }
@@ -49,6 +48,15 @@ constructor(){
  
 }
 
+addTodo = (todoItem) =>{
+  const newTodo ={
+    id:uuidv4(),
+    item: todoItem,
+    completed:false
+  }
+this.setState({todo:[...this.state.todo, newTodo]})
+}
+
 render(){
     return(
         <div className="App">
@@ -56,7 +64,7 @@ render(){
           <h1 className="text-center">Todo List</h1>
           </header>
           
-          <AddNewToDo />
+          <AddNewToDo addTodo={this.addTodo}/>
           
           {/*Pass todo to the list*/ }
           <TodoList addTodo={this.state.todo} deleteTodo={this.deleteTodo}>
--- src/components/addNewToDo.js
+++ src/components/addNewToDo.js
@@ -1,32 +1,54 @@
 import React, { Component } from 'react'
 import { Button,Form } from 'react-bootstrap';
 
-class addNewToDo extends Component {
+class AddNewToDo extends Component {
     constructor(){
         super();
         this.state={
-            inputTerm:''
+            newTodo:'',
+            submitDisabled:true
         }
     }
 
-    
+    onChange =(e) =>{
+        //Se periptosh pou eixame forma me polla input mporoume na orizoume to name tou input idio me to ta state  wste na mh xreiazetai na kanume event handler gia ola ta input
+        this.setState({[e.target.name]:e.target.value});
+
+         /*An exoume ena mono input
+       // this.setState({newTodo:e.target.value});
+       // console.log(e.target.value);*/
+       
+        if(e.target.value!==''){
+            this.setState({submitDisabled:false})
+        }
+       
+    }
+    onSubmit =(e)=>{
+        e.preventDefault(); //apagoreuoyme na ginei to submit sto idio arxeio AddNewToDo.js
+        this.props.addTodo(this.state.newTodo);
+        this.setState({newTodo:''});
+        this.setState({submitDisabled:true});
+    }
+
     render() {
         return (
-            <Form  style={{padding:'10px'}}>
+            <div style={{margin:"auto",width:'95%'}}>
+            <Form  style={{padding:'10px'}} onSubmit={this.onSubmit}>
                 <Form.Row className='d-flex bd-highlight'>
                     <Form.Group className="col-10" controlId="newTodo">
-                    <Form.Control type="text" placeholder="Enter a new todo" />
+                    <Form.Control name='newTodo' value={this.state.newTodo} type="text" placeholder="Enter a new todo" onChange={this.onChange}/>
                     </Form.Group>
 
                     <Form.Group >
-                    <Button variant="primary" type="submit" >
+                    <Button variant="primary" type="submit" disabled={this.state.submitDisabled}>
                     <i className="fas fa-plus"></i> Add new Todo
                     </Button>
                     </Form.Group>
                 </Form.Row>
             </Form>
+           </div>
 
         )
     }
 }
-export default addNewToDo;
+export default AddNewToDo;
